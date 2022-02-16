c6e6bd3da455bd81a7d4e18fd3915998942989a5
AdarshNair001 <adn96nair@csu.fullerton.edu>
2022-02-16 13:21:51 -0800

    asda

--- 2_9/index.php
+++ 2_9/index.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web_App_InClass</title>
+    <link rel="Stylesheet" href="css/home.css" />
 </head>
+
 <body>
     <header>
         <?php echo "Today is  " . date("Y/m/d") . "<br>"; ?>
@@ -15,22 +17,25 @@
         <h1>Hello_World</h1>
         <button class="say-hi button" data-message="Hi" type="button">Click Me</button>
     </main>
+
     <footer class="cool-footer">Copyright 2022</footer>  
+
     <script>
         function SayHi(event){
-            let eventSource = event.target;
+            let eventSource = event.target;                                 //it remembers the items clicked, in this case everything on the HTML page
 
-            if(!eventSource.classList.contain("say-hi-button")) return
+            if(!eventSource.classList.contain("say-hi-button")) return;     //this filters clicking on the button, hence clicking on other items(ie: h1 or footer) on the page wont be detected
 
             let displayText = eventSource.dataset.message ?? "No Message Available";
 
             alert(disaplyText);
         }
         document.addEventListener("click", SayHi)
+
         let MyVat ={
             name: "bob",
             age: 22
-        };
+        }
         
     </script>   
 </body>
